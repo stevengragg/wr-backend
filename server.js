9b3c41a37260a11d38776a198a083c2e2ed92d62
sgragg5619 <sgragg5619@gmail.com>
2021-12-13 05:52:46 +0800

    Set server and env

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1/restaurants", restaurants);
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+
+export default app;
